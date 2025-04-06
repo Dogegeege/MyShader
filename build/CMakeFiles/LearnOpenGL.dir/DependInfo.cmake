05a7894b9115b52eb34bee6c3c282f4283e24810
Dog_E <1061116935@qq.com>
2025-04-06 16:37:52 +0800

    newfile

---
+++ build/CMakeFiles/LearnOpenGL.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Project/Learn_OpenGL/include/glad/glad.c" "CMakeFiles/LearnOpenGL.dir/include/glad/glad.c.obj" "gcc" "CMakeFiles/LearnOpenGL.dir/include/glad/glad.c.obj.d"
+  "E:/Project/Learn_OpenGL/include/stb_image/stb_image.cpp" "CMakeFiles/LearnOpenGL.dir/include/stb_image/stb_image.cpp.obj" "gcc" "CMakeFiles/LearnOpenGL.dir/include/stb_image/stb_image.cpp.obj.d"
+  "E:/Project/Learn_OpenGL/src/camera.cpp" "CMakeFiles/LearnOpenGL.dir/src/camera.cpp.obj" "gcc" "CMakeFiles/LearnOpenGL.dir/src/camera.cpp.obj.d"
+  "E:/Project/Learn_OpenGL/src/main.cpp" "CMakeFiles/LearnOpenGL.dir/src/main.cpp.obj" "gcc" "CMakeFiles/LearnOpenGL.dir/src/main.cpp.obj.d"
+  "E:/Project/Learn_OpenGL/src/model.cpp" "CMakeFiles/LearnOpenGL.dir/src/model.cpp.obj" "gcc" "CMakeFiles/LearnOpenGL.dir/src/model.cpp.obj.d"
+  "E:/Project/Learn_OpenGL/src/shader.cpp" "CMakeFiles/LearnOpenGL.dir/src/shader.cpp.obj" "gcc" "CMakeFiles/LearnOpenGL.dir/src/shader.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
