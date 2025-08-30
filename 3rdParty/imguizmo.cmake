dccd8c2c96fa8ec5736fd9767ca06eae54f11f5a
Dog_E <1061116935@qq.com>
2025-08-30 14:58:06 +0800

    test

--- 3rdParty/imguizmo.cmake
+++ 3rdParty/imguizmo.cmake
@@ -1,11 +1,24 @@
-set(imguizmo_SOURCE_DIR_ ${CMAKE_CURRENT_SOURCE_DIR}/imguizmo)
-set(LIBRARY_NAME imguizmo)
+set (imguizmo_SOURCE_DIR_ ${CMAKE_CURRENT_SOURCE_DIR}/imguizmo)
+set (LIBRARY_NAME imguizmo)
 
-file(GLOB imguizmo_sources CONFIGURE_DEPENDS
-    "${imguizmo_SOURCE_DIR_}/ImGuizmo.cpp"
-    "${imguizmo_SOURCE_DIR_}/ImGuizmo.h"
-    )#主文件
+# add_library (ImGui SHARED
+# ${imguizmo_SOURCE_DIR_}/example/imgui.h
+# ${imguizmo_SOURCE_DIR_}/example/imgui.cpp
+# )
+add_library (${LIBRARY_NAME} SHARED
+    ${imguizmo_SOURCE_DIR_}/ImGuizmo.cpp
+    ${imguizmo_SOURCE_DIR_}/ImGuizmo.h
 
-add_library(${LIBRARY_NAME} SHARED ${imguizmo_sources} )
-target_include_directories(${LIBRARY_NAME}  PUBLIC  ${imguizmo_SOURCE_DIR_} )
-target_link_libraries(${LIBRARY_NAME}  PUBLIC imgui)
+    ${CMAKE_CURRENT_SOURCE_DIR}/imgui-docking/imgui.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/imgui-docking/imgui.cpp
+
+    # ${imguizmo_SOURCE_DIR_}/example/ImApp.h
+    # ${imguizmo_SOURCE_DIR_}/example/main.cpp
+)
+target_include_directories (${LIBRARY_NAME} PUBLIC
+    ${imguizmo_SOURCE_DIR_}
+    ${imguizmo_SOURCE_DIR_}/example
+    ${CMAKE_CURRENT_SOURCE_DIR}/imgui-docking
+)
+
+target_link_libraries (${LIBRARY_NAME} PUBLIC imgui glfw)
